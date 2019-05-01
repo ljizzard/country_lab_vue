0a401facd32c93e129c53034a562fce6228302a3
lindsey <lj0978@hotmail.co.uk>
2019-05-01 20:19:30 +0100

    initial commit - working MVP

---
+++ src/components/CountryDetail.vue
@@ -0,0 +1,20 @@
+<template lang="html">
+<div v-if="country">
+  <p>name: {{country.name}}</p>
+  <p>capital: {{country.capital}}</p>
+  <p>population: {{country.population}}</p>
+
+</div>
+
+
+</template>
+
+<script>
+export default {
+  name: 'country-detail',
+  props: ['country']
+}
+</script>
+
+<style lang="css" scoped>
+</style>
---
+++ src/components/CountryList.vue
@@ -0,0 +1,23 @@
+<template lang="html">
+<div>
+  <ul>
+    <list-item v-for="(country, index) in countries" :country="country" :key="index"></list-item>
+  </ul>
+
+</div>
+</template>
+
+<script>
+import listItem from './listItem.vue';
+
+export default {
+  name: 'country-list',
+  props: ['countries'],
+  components: {
+    "list-item": listItem
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
